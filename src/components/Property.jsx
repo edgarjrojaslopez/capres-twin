d156abee4832a38f55d1ffeeec5f81672bc4f0fd
Edgar Rojas <edgarjrojasl@gmail.com>
2023-05-06 18:29:41 -0400

    Adding details

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -4,6 +4,8 @@ import { FaBed, FaBath } from 'react-icons/fa'
 import { BsGridFill } from 'react-icons/bs'
 import { GoVerified } from 'react-icons/go'
 import millify from 'millify'
+import defaultImage from '../assets/images/homePhoto.jpg'
+import tw from 'twin.macro'
 
 const Property = ({
   property: {
@@ -20,7 +22,47 @@ const Property = ({
   },
 }) => (
   <Link href={`/property/${externalID}`} passHref>
-    {title}
+    <div tw="flex flex-wrap w-[420px] p-[5px] pt-0 justify-start cursor-pointer">
+      <div>
+        <Image
+          width={400}
+          height={260}
+          src={coverPhoto ? coverPhoto.url : defaultImage}
+          alt="house"
+        />
+      </div>
+      <div tw="w-full">
+        <div tw="flex pt-[2px] items-center justify-between">
+          <div tw="flex items-center">
+            <div tw="pr-[3px] text-green-400 ">
+              {isVerified && <GoVerified />}{' '}
+            </div>
+            <div tw="font-bold text-lg">
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}{' '}
+            </div>
+          </div>
+          <div>
+            <div tw="mt-3 h-16 w-16 inline-block rounded-full ring-2 ring-white">
+              <Image
+                tw=""
+                src={agency?.logo?.url}
+                alt="agency"
+                width={150}
+                height={200}
+              />
+            </div>
+          </div>
+        </div>
+        <div tw="flex items-center p-[1px] justify-between w-[250px] text-blue-400">
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{' '}
+          <BsGridFill />
+        </div>
+        <div tw="text-lg">
+          {title.lenght > 30 ? `${title.substring(0, 30)}...` : title}
+        </div>
+      </div>
+    </div>
   </Link>
 )
 export default Property
--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ export default function Home({ propertiesForRent, propertiesForSale }) {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <div tw="flex flex-wrap">
+      <div tw="flex flex-wrap gap-2">
         {propertiesForRent.map(property => (
           <Property property={property} key={property.id} />
         ))}
@@ -71,7 +71,7 @@ export default function Home({ propertiesForRent, propertiesForSale }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      <div tw="flex flex-wrap">
+      <div tw="flex flex-wrap gap-2">
         {propertiesForSale.map(property => (
           <Property property={property} key={property.id} />
         ))}
